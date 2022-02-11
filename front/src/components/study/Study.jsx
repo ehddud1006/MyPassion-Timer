133a33e3e91bc3031035a3b8de92d91da46497f4
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-02-11 23:31:09 +0900

    dd

--- front/src/components/study/Study.jsx
+++ front/src/components/study/Study.jsx
@@ -43,9 +43,11 @@ function Study() {
             })
             console.log("hh")
             console.log(res.data[0])
+            console.log(res.data[0].time)
             setTime(res.data[0].time)
             setId(res.data[0]._id)
             total_studied = time
+            console.log(total_studied)
             // console.log(res)
             // setPosts(res.data);
             // {data: Array(3), status: 200, statusText: 'OK', headers: {…}, config: {…}, …}
