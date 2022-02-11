663e6553db808d9e5e0c79172c8571eb2f64cf5b
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-02-11 22:53:42 +0900

    dd

--- back/index.js
+++ back/index.js
@@ -62,9 +62,9 @@ app.get('/*', function (req, res) {
 
 
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-app.listen(5000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    // app.listen(5000, () => {
     console.log("Backend is running.");
 });
 
