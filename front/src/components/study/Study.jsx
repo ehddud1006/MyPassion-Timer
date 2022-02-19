0351f176ec7f9f98fcda3cb44998279a239bf092
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-02-20 04:35:41 +0900

    dada

--- front/src/components/study/Study.jsx
+++ front/src/components/study/Study.jsx
@@ -351,7 +351,7 @@ function Study() {
 
     // convenience function to setup a webcam
     const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(100, 100, flip); // width, height, flip
+    let webcam = new tmImage.webcam(1800, 800, flip);// width, height, flip
     await webcam.setup({ facingMode: "environment" }); // use "user" to use front-cam on mobile phones
 
     // append elements to the DOM --> **before starting the webcam**
