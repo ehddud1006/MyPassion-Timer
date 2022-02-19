46b4c13ef24466cd9bd967f041d6d9ed0a8e6246
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-02-20 05:22:23 +0900

    dada

--- front/src/components/study/Study.jsx
+++ front/src/components/study/Study.jsx
@@ -338,39 +338,28 @@ function Study() {
 
     // console.log(check);
     // console.log("total" + total_studied);
-
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
     prev_time = new Date();
     offset_time = new Date();
 
-    const modelURL = URL + 'model.json';
-    const metadataURL = URL + 'metadata.json';
-
     // load the model and metadata
     // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
     // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
     model = await tmImage.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
 
-    // convenience function to setup a webcam
+    // Convenience function to setup a webcam
     const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(180, 100, flip); // width, height, flip
-    await webcam.setup({ facingMode: "user" }); // use "user" to use front-cam on mobile phones
+    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
 
-    // append elements to the DOM --> **before starting the webcam**
-    // document.getElementById('webcam-container').appendChild(webcam.canvas); // just in case you want to use specifically the canvas
-    document.getElementById('webcam-container').appendChild(webcam.webcam); // webcam object needs to be added in any case to make this work on iOS
-
-    // grab video-object in any way you want and set the attributes --> **"muted" and "playsinline"**
-    let wc = document.getElementsByTagName('video')[0];
-    wc.setAttribute("playsinline", true); // written with "setAttribute" bc. iOS buggs otherwise :-)
-    wc.muted = "true"
-    wc.id = "webcamVideo";
-
-    // only now start the webcam --> **after video-object added to DOM and attributes are set**
-    webcam.play();
-    window.requestAnimationFrame(loop); // update canvas by loop-function
-
+    // append elements to the DOM
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
     labelContainer = document.getElementById("label-container");
     for (let i = 0; i < maxPredictions; i++) {
       // and class labels
